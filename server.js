6e32763fd6f26673d2bcb89e1b471a3fe973b9ef
Weslley Braga <wesbragagt@gmail.com>
2019-05-25 12:20:00 -0500

    adding notes but updating all jobs that have notes

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ app.set("view engine", "handlebars");
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/jobScrape";
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
 
 // Require routes
 // HOME PAGE ROUTE
